1fd724ffe930c39c5cf0c66f6f9cc8be2857aec8
Aswathy-A-S <155232817+Aswathy-A-S@users.noreply.github.com>
2024-08-31 17:46:00 +0530

    Create admin.py

---
+++ admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+
+admin.site.register(Farmeraddcategory)
+admin.site.register(Productorder)
+admin.site.register(Login)
+admin.site.register(Customer)
+admin.site.register(Booksubcription)
+admin.site.register(Distributor)
